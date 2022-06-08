8723995d766c905c54ad76680ed30312748e5f28
andycodev <andycodev@gmail.com>
2022-06-08 17:08:48 -0500

    deploying

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,18 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+mv dist docs
 
-git init
-git add -A
-git commit -m 'deploy'
+# navigate into the build output directory
+git add .
+#git add docs -f
+
+git commit -m "deploying"
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:andycodev/deploy-vue-cli-test.git master:gh-pages
 # git push -f https://github.com/andycodev/deploy-vue-cli-test.git master:gh-pages
 
-git subtree push -f https://github.com/andycodev/deploy-vue-cli-test.git --prefix dist origin gh-pages
+git push
 
 cd -
