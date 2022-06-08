08801ccfd1d765cadb23a174c77ed51b929fc253
andycodev <andycodev@gmail.com>
2022-06-08 18:05:31 -0500

    deploying

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,27 @@
+# Deploy production
+
 #!/usr/bin/env sh
 
 # abort on errors
 set -e
 
+# Verificamos que si existe directorio docs
 if [ -d docs ]
-
 then
-
+  # Si existe directorio lo eliminamos
    rm -r docs
    echo "El directorio docs eliminado"
 
 fi
+  # Si no existe directorio dos
 
+  # Contruimos para producción
    npm run build
 
-   # build
+   # Renombramos la carpeta dist por docs
    mv dist docs
 
+   # Agregamos todos los archivos modificados al repo
    git add .
 
    git commit -m "deploying"
